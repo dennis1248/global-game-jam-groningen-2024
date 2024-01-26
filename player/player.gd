7510b2c5aa1d1b8e09ec6b7806da85d17a374936
Dennis ten Hoove <dennistenhoove@protonmail.com>
2024-01-26 23:40:54 +0100

    Add player animation

--- player/player.gd
+++ player/player.gd
@@ -5,6 +5,17 @@ const SPEED = 300.0
 func _physics_process(_delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 
+	if velocity.x > 0:
+		%AnimationPlayer.play("walk_right")
+	elif velocity.x < 0:
+		%AnimationPlayer.play("walk_left")
+	elif velocity.y > 0:
+		%AnimationPlayer.play("walk_down")
+	elif velocity.y < 0:
+		%AnimationPlayer.play("walk_up")
+	else:
+		%AnimationPlayer.play("idle")
+
 	velocity = direction * SPEED
 	
 	move_and_slide()
